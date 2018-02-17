7d81189075be361802a2171e5fa9d4e9f6e27d8b
Imran Khan <ik21191@gmail.com>
2018-02-17 21:01:13 +0530

    [v1]:Some changes

--- src/main/java/com/mypack/beans/User.java
+++ src/main/java/com/mypack/beans/User.java
@@ -1,8 +1,11 @@
 package com.mypack.beans;
 
+import java.io.Serializable;
 import java.util.Set;
 
-public class User {
+public class User implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 	private int id;
 	private String userid;
 	private String password;
